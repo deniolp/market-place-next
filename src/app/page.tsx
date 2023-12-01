5f185773527d79c6cf8e4272b64f2e0624585e91
Denis Popov <denis@pixeladas.es>
2023-12-01 15:06:34 +0100

    Using Button ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { Button, buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function Home() {
@@ -14,9 +15,13 @@ export default function Home() {
 					our team to ensure our highest quality standartds.
 				</p>
 				<div className='flex flex-col sm:flex-row gap-4 mt-6'>
-					<Link href='/products'>Browse Trending</Link>
+					<Link href='/products' className={buttonVariants()}>
+						Browse Trending
+					</Link>
+					<Button variant='ghost'>Our quality promise &rarr;</Button>
 				</div>
 			</div>
+			{/* TODO: List of products */}
 		</MaxWidthWrapper>
 	);
 }
