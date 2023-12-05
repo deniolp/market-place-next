feb82ab6396624dc8f991e9164a2e9a420e05a37
Denis Popov <denis@pixeladas.es>
2023-12-05 10:25:23 +0100

    Adding items to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { Icons } from './Icons';
+import NavItems from './NavItems';
 
 const Navbar = () => {
 	return (
@@ -10,9 +11,16 @@ const Navbar = () => {
 					<div className='border-b border-gray-200'>
 						<div className='flex h-16 items-center'>
 							{/* TODO: Mobile nav */}
-							<Link href='/'>
-								<Icons.logo className='h-10 w-10' />
-							</Link>
+
+							<div className='ml-4 flex lg:ml-0'>
+								<Link href='/'>
+									<Icons.logo className='h-10 w-10' />
+								</Link>
+							</div>
+
+							<div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+								<NavItems />
+							</div>
 						</div>
 					</div>
 				</MaxWidthWrapper>
